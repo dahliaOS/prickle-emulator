547cc313111228c94814170e2422d5faedf84235
EnderNightLord <camden.o.b@gmail.com>
2019-05-13 16:41:29 +1200

    Edit Ubuntu Install Scripts

--- lib/qemu/install-ubuntu.sh
+++ lib/qemu/install-ubuntu.sh
@@ -1,3 +1,3 @@
 dd of=ubuntu.img bs=1024 seek=10194304 count=0
-./qemu-system-x86_64 -hda ubuntu.img -cdrom mini.iso -m 1000 -boot d
+./x86_64-softmmu/qemu-system-x86_64 -hda ubuntu.img -cdrom mini.iso -m 1000 -boot d
 
--- lib/qemu/run-ubuntu.sh
+++ lib/qemu/run-ubuntu.sh
@@ -1,1 +1,1 @@
-./qemu-system-x86_64 -hda ubuntu.img -m 1000 -boot d
+./x86_64-softmmu/qemu-system-x86_64 -hda ubuntu.img -m 1000 -boot d
